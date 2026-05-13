9513f7980e811f9e103b3adf86a8160b9b95761c
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-05-13 17:17:58 -0300

    Adicionando exercícios da Aula 02

--- Senac/Aula02/Aula02/Program.cs
+++ Senac/Aula02/Aula02/Program.cs
@@ -10,6 +10,23 @@ namespace Aula02
     {
         static void Main(string[] args)
         {
+            int num = 10;
+            if (num == 10)
+            {
+                Console.WriteLine("Igual a 10");
+            }
+            else if (num == 20)
+            {
+                Console.WriteLine("Igual a 20");
+            }
+            else if (num == 30)
+            {
+                Console.WriteLine("Igual a 30");
+            }
+            else
+            {
+                Console.WriteLine("Diferente de 10, 20, 30");
+            }
         }
     }
 }
