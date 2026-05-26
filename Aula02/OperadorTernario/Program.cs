2d523aeb959f737978eda7d5b02e5cc9687cc29c
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-05-26 14:56:18 -0300

    Arrumando Repositório

---
+++ Aula02/OperadorTernario/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadorTernario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(numero % 2 == 0 ? "Par" : "Ímpar");
+            Console.WriteLine(numero == 10 ? "Igual a dez" : "Diferente de dez");
+
+        }
+    }
+}
---
+++ Aula02/TentativaSenha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TentativaSenha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Senha correta do sistema
+            string senhaCorreta = "1234";
+
+            //Variável para armazenar a senha digitada
+            string senhaDigitada = "";
+
+            //Repete enquanto a senha estiver incorreta
+            while (senhaDigitada != senhaCorreta)
+            {
+                Console.WriteLine("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+
+                if(senhaDigitada == senhaCorreta)
+                {
+                    Console.WriteLine("Acesso permitido!");
+                }
+                else
+                {
+                    Console.WriteLine("Senha Incorreta. Tente Novamente.\n");
+                }
+            }
+
+        }
+    }
+}
